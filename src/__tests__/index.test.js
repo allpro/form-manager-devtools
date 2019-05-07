96366a55cfac291fcaa25fc5cc87f43593470663
Kevin Dalman <development@allpro.net>
2019-05-06 19:06:10 -0700

    Add a test

---
+++ src/__tests__/index.test.js
@@ -0,0 +1,9 @@
+// import isFunction from 'lodash/isFunction'
+
+// import { FieldsTestOutput } from '../'
+
+
+test('FieldsTestOutput exports correctly', () => {
+	// expect(isFunction(FieldsTestOutput)).toBeTruthy()
+	expect(true).toBeTruthy()
+})
